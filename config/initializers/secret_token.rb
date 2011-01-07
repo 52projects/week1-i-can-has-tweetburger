6766d08087eb9e2cd165448df31a5b9b145226e0
Nick Floyd <nicholas.floyd@gmail.com>
2011-01-07 07:25:55 -0600

    dropped the hammer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icanhastweetburger::Application.config.secret_token = '19a5a0e733f5991f5d54c559686f4e6cc98b33cc55c0c35a288f98d4a192fa159674f3947f6ab67e85af437486501eecb8f2e53ccd2f4d06f5fc555b88fa4404'
