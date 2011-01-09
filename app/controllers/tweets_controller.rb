7eb4df5cb6955233e9b9efd5630c7e54d28310f8
Nick Floyd <nicholas.floyd@gmail.com>
2011-01-08 21:17:08 -0600

    Added preview lol cat image, def needs rework, just getting it working to see what it looks like

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,7 +14,38 @@ class TweetsController < ApplicationController
   end
   
   def create
+    
     @loltweet = params[:tweet].to_lolspeak
+    
+    #POST to: http://cheezburger.com/caption/previewcaption.ashx
+    #URI.escape()
+    xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><CaptionData><OriginalImageUrl>" + params[:original_image_url] + "</OriginalImageUrl><Captions><Caption><Text>" + @loltweet + "</Text><FontFamily>Impact</FontFamily><FontSize>40</FontSize><FontColor>white</FontColor><XPosition>80</XPosition><YPosition>50</YPosition><IsBold>false</IsBold><TextStyle>outline</TextStyle><IsItalic>false</IsItalic><IsStrikeThrough>false</IsStrikeThrough><IsUnderLine>false</IsUnderLine><Opacity>100</Opacity></Caption></Captions></CaptionData>"
+
+    
+     # xml = string.multiline do
+     #  "<?xml version='1.0' encoding='utf-8'?>"
+     #  "<CaptionData>"
+     #    "<OriginalImageUrl>" + params[:original_image_url] + "</OriginalImageUrl>"
+     #    "<Captions>"
+     #      "<Caption>"
+     #        "<Text>" + @loltweet + "</Text>"
+     #        "<FontFamily>Impact</FontFamily>"
+     #        "<FontSize>40</FontSize>"
+     #        "<FontColor>white</FontColor>"
+     #        "<XPosition>80</XPosition>"
+     #        "<YPosition>205</YPosition>"
+     #        "<IsBold>false</IsBold>"
+     #        "<TextStyle>outline</TextStyle>"
+     #        "<IsItalic>false</IsItalic>"
+     #        "<IsStrikeThrough>false</IsStrikeThrough>"
+     #        "<IsUnderLine>false</IsUnderLine>"
+     #        "<Opacity>100</Opacity>"
+     #      "</Caption>"
+     #    "</Captions>"
+     #  "</CaptionData>"
+     #  end
+    
+    @previewimage = "http://cheezburger.com/caption/previewcaption.ashx?" + CGI.escape(xml)
     
   end
 end
